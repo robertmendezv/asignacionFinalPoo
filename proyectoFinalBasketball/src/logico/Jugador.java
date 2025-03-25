f03890078a679e2ef7949d5d27771859f580e17e
robertmendezv <rdmv0001@ce.pucmm.edu.do>
2025-03-25 13:30:58 -0400

    corregi la clase Lesion y Jugador

--- proyectoFinalBasketball/src/logico/Jugador.java
+++ proyectoFinalBasketball/src/logico/Jugador.java
@@ -4,6 +4,50 @@ public class Jugador {
 	private String nombreJugador;
 	private int edad;
 	private String posicionJugador;
+	private int numJugador;
+	private Estadisticas estadistica;
+	public Jugador(String nombreJugador, int edad, String posicionJugador, int numJugador, Estadisticas estadisticas) {
+		super();
+		this.nombreJugador = nombreJugador;
+		this.edad = edad;
+		this.posicionJugador = posicionJugador;
+		this.numJugador = numJugador;
+		this.estadistica = estadistica;
+	}
+	public String getNombreJugador() {
+		return nombreJugador;
+	}
+	public void setNombreJugador(String nombreJugador) {
+		this.nombreJugador = nombreJugador;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getPosicionJugador() {
+		return posicionJugador;
+	}
+	public void setPosicionJugador(String posicionJugador) {
+		this.posicionJugador = posicionJugador;
+	}
+	public int getNumJugador() {
+		return numJugador;
+	}
+	public void setNumJugador(int numJugador) {
+		this.numJugador = numJugador;
+	}
+	public Estadisticas getEstadistica() {
+		return estadistica;
+	}
+	public void setEstadistica(Estadisticas estadistica) {
+		this.estadistica = estadistica;
+	}
+
+	
+	
+	
 	
 	
 }
--- proyectoFinalBasketball/src/logico/Lesion.java
+++ proyectoFinalBasketball/src/logico/Lesion.java
@@ -1,5 +1,33 @@
 package logico;
 
 public class Lesion {
-
+	private String tipoLesion;
+	private int duracion;
+	private Jugador jugadorLesionado;
+	public Lesion(String tipoLesion, int duracion, Jugador jugadorLesionado) {
+		super();
+		this.tipoLesion = tipoLesion;
+		this.duracion = duracion;
+		this.jugadorLesionado = jugadorLesionado;
+	}
+	public String getTipoLesion() {
+		return tipoLesion;
+	}
+	public void setTipoLesion(String tipoLesion) {
+		this.tipoLesion = tipoLesion;
+	}
+	public int getDuracion() {
+		return duracion;
+	}
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+	public Jugador getJugadorLesionado() {
+		return jugadorLesionado;
+	}
+	public void setJugadorLesionado(Jugador jugadorLesionado) {
+		this.jugadorLesionado = jugadorLesionado;
+	}
+	
+	
 }
